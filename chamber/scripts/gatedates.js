f26a4daba8ecc68ace4fe4173db0edc1b0f57c10
cdardong-byui <dar22009@byui.edu>
2024-04-30 21:55:05 -0600

    chamber

---
+++ chamber/scripts/gatedates.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("lastModified").innerHTML =  document.lastModified;
+
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+Name = "Cristian Joaquin Dardon Godoy";
